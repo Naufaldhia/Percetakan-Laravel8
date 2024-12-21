9212288a8d295177ce7388afaadde6e1688971b7
Naufaldhia <naufaldhia15@gmail.com>
2024-12-21 07:24:18 +0700

    Memasukkan Data Tabel ke dalam Database

--- database/migrations/2024_12_21_000918_create_pengaturans_table.php
+++ database/migrations/2024_12_21_000918_create_pengaturans_table.php
@@ -10,9 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    { // Memasukkan Data Tabel ke dalam Database
+        // php artisan migrate
         Schema::create('pengaturans', function (Blueprint $table) {
             $table->id();
+            $table->date('tanggal');
+            $table->string('lokasi');
+            $table->string('kepada');
+            $table->string('bon_faktur');
+            $table->integer('banyak_barang');
+            $table->enum('jenis_barang', ['lembar', 'rim']);
+            $table->string('nama_barang');
+            $table->integer('harga');
+            $table->integer('jumlah');
             $table->timestamps();
         });
     }
