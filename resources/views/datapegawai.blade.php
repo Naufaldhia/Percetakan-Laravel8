0c0a9357bf6a42b76db746cbcf28ead10435930d
Naufaldhia <naufaldhia15@gmail.com>
2024-12-21 08:09:40 +0700

    Pengisian datapegawai.blade.php

--- resources/views/datapegawai.blade.php
+++ resources/views/datapegawai.blade.php
@@ -3,11 +3,50 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Percetakan Adi Prima</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1 class="text-center mb-4">Data Kwitansi</h1>
+    <div class="container">
+    <button type="button" class="btn btn-success">Tambah Data</button>
+        <div class="row">
+            <!-- Pemanggilan Table dari Bootstrap -->
+    <table class="table table-striped table-hover">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Tanggal</th>
+      <th scope="col">Lokasi</th>
+      <th scope="col">Kepada</th>
+      <th scope="col">Bon/Faktur</th>
+      <th scope="col">Banyak Barang</th>
+      <th scope="col">Jenis Barang</th>
+      <th scope="col">Nama Barang</th>
+      <th scope="col">Harga</th>
+      <th scope="col">Jumlah</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>
+      <button type="button" class="btn btn-danger">Delete</button>
+      <button type="button" class="btn btn-info">Info</button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
